372e77e2078c99764144c9eb43fcc813c9d57950
Elissandro Caetano Júnior <ecjunior19@gmail.com>
2024-05-18 13:50:21 -0300

    feat(stack): Implementação e testes da classe Stack

---
+++ src/stack.js
@@ -0,0 +1,57 @@
+class Stack {
+
+    constructor() {
+        this.items = [];
+    }
+
+    // Coloca elemento no topo da pilha
+    push(element) {
+
+        this.items.push(element);
+
+    }
+
+    // Pega elemento do topo e o remove da pilha
+    pop() {
+
+        // Underflow se a pilha está vazia
+        if (this.isEmpty()) {
+            return "Pilha vazia";
+        }
+
+        return this.items.pop();
+    }
+
+    // Retorna elemento do topo
+    peek() {
+
+        if (this.isEmpty()) {
+            return "Pilha vazia";
+        }
+
+        return this.items[this.items.length - 1];
+    }
+
+    // Verifica se a pilha está vazia
+    isEmpty() {
+        return this.items.length == 0;
+    }
+
+    //Tamanho da Stack
+    size() {
+        return this.items.length
+    }
+
+    // Imprime a Stack
+    printStack() {
+        let str = "";
+        for (let i = 0; i < this.items.length; i++) {
+            str += this.items[i];
+            if(i != this.items.length - 1) str += " ";
+        }
+        return str;
+    }
+
+}
+
+module.exports = Stack;
---
+++ tests/stack.test.js
@@ -0,0 +1,56 @@
+const Stack = require('../src/stack'); // Importar a classe Stack
+
+describe('Stack', () => {
+  let stack; // Variável para armazenar instância da pilha
+
+  beforeEach(() => {
+    stack = new Stack(); // Criar nova instância da pilha antes de cada teste
+  });
+
+  test('deve ser criada vazia', () => {
+    expect(stack.isEmpty()).toBe(true);
+  });
+
+  test('deve empilhar elementos', () => {
+    stack.push(10);
+    stack.push(20);
+    expect(stack.size()).toBe(2);
+  });
+
+  test('deve desempilhar elementos', () => {
+    stack.push(10);
+    stack.push(20);
+    expect(stack.pop()).toBe(20);
+    expect(stack.pop()).toBe(10);
+  });
+
+  test('deve retornar "Pilha vazia" ao desempilhar de uma pilha vazia', () => {
+    expect(stack.pop()).toBe("Pilha vazia");
+  });
+
+  test('deve retornar o elemento do topo sem removê-lo', () => {
+    stack.push(10);
+    stack.push(20);
+    expect(stack.peek()).toBe(20);
+    expect(stack.size()).toBe(2);
+  });
+
+  test('deve retornar "Pilha vazia" ao pegar o elemento do topo de uma pilha vazia', () => {
+    expect(stack.peek()).toBe("Pilha vazia");
+  });
+
+  test('deve indicar quando está vazia', () => {
+    expect(stack.isEmpty()).toBe(true);
+    stack.push(10);
+    stack.pop();
+    expect(stack.isEmpty()).toBe(true);
+  });
+
+  test('deve imprimir a pilha corretamente', () => {
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+    expect(stack.printStack()).toBe("10 20 30");
+  });
+
+});

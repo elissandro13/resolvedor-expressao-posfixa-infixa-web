f80070082ec2330eb377820df5398a0ea06e3fa5
Matheus Vaz <vazleal@outlook.com>
2024-05-19 17:55:55 -0400

    Create a react app and use the expressions resolver script

---
+++ calculadora/src/components/Calculator.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect, useRef } from 'react';
+import Button from './Button';
+import Display from './Display';
+import './Calculator.css';
+import { postFixEvaluation, inFixEvaluation } from '../utils/resolveExpressoes';
+
+const Calculator = () => {
+  const [input, setInput] = useState('');
+  const [mode, setMode] = useState('infix');
+  const displayRef = useRef(null);
+
+  useEffect(() => {
+    const display = displayRef.current;
+    if (display) {
+      if (display.scrollWidth > display.clientWidth) {
+        display.style.height = `${display.scrollHeight}px`;
+      } else {
+        display.style.height = 'auto';
+      }
+    }
+  }, [input]);
+
+  const handleClick = (value) => {
+    if (value === '=') {
+      try {
+        if (mode === 'postfix') {
+          setInput(postFixEvaluation(input).toString());
+        } else if (mode === 'infix') {
+          setInput(inFixEvaluation(input).toString());
+        }
+      } catch (error) {
+        setInput('Error');
+      }
+    } else if (value === 'C') {
+      setInput('');
+    } else if (value === 'PostFix') {
+      setMode('postfix');
+    } else if (value === 'InFix') {
+      setMode('infix');
+    } else if (value === 'Espaço') {
+      setInput(input + ' ');
+    } else if (value === 'Backspace') {
+      setInput(input.slice(0, -1));
+    } else {
+      setInput(input + value);
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <Display value={input} ref={displayRef} />
+      <div className="buttons">
+        {['7', '8', '9', '/'].map((val) => (
+          <Button key={val} value={val} onClick={handleClick} />
+        ))}
+        {['4', '5', '6', '*'].map((val) => (
+          <Button key={val} value={val} onClick={handleClick} />
+        ))}
+        {['1', '2', '3', '-'].map((val) => (
+          <Button key={val} value={val} onClick={handleClick} />
+        ))}
+        {['0', '.', '=', '+'].map((val) => (
+          <Button key={val} value={val} onClick={handleClick} />
+        ))}
+        {['(', ')', 'C', 'Espaço'].map((val) => (
+          <Button key={val} value={val} onClick={handleClick} />
+        ))}
+        <Button value="PostFix" onClick={handleClick} />
+        <Button value="InFix" onClick={handleClick} />
+        <Button value="Backspace" onClick={handleClick} />
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
--- calculadora/src/utils/index.js
+++ calculadora/src/utils/index.js
@@ -1,8 +1,8 @@
-const { postFixEvaluation } = require("./resolveExpressoes");
+const { postFixEvaluation } = require('./resolveExpressoes');
 
 let expression = "10 5 / 2 3 -";
-let resultado = postFixEvaluation(expression);
-console.log(resultado); // Deve imprimir o resultado da expressão
+//let resultado = postFixEvaluation(expression);
+//console.log(resultado); // Deve imprimir o resultado da expressão
 
 expression = "2 3 4 * 5 6 / + 10 -";
 resultado = postFixEvaluation(expression);
